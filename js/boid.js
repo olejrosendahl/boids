000d062eba0e4dffbc5a18c34d37f10f24c29d2b
Ole J. Rosendahl <ole.johnny.rosendahl@gmail.com>
2015-05-23 01:33:32 +0700

    Avoid walls, add alignment

--- js/boid.js
+++ js/boid.js
@@ -5,6 +5,11 @@ var Boid = function() {
   this.velocity = new THREE.Vector3();
   _acceleration = new THREE.Vector3();
 
+  var _width = 500,
+    _height = 500,
+    _depth = 200,
+    _neighborhoodRadius = 100;
+
   this.separation = function ( boids ) {
     var boid, distance,
     posSum = new THREE.Vector3(),
@@ -27,39 +32,39 @@ var Boid = function() {
     return posSum;
   }
 
-  this.cohesion = function ( boids ) {
+  this.cohesion = function (boids) {
     var boid, distance,
     posSum = new THREE.Vector3(),
     steer = new THREE.Vector3(),
     count = 0;
 
-    for ( var i = 0, il = boids.length; i < il; i ++ ) {
-      if ( Math.random() > 0.6 ) continue;
+    for (var i = 0, il = boids.length; i < il; i++) {
+      if (Math.random() > 0.6) continue;
 
       boid = boids[ i ];
-      distance = boid.position.distanceTo( this.position );
+      distance = boid.position.distanceTo(this.position);
 
-      if ( distance > 0 && distance <= _neighborhoodRadius ) {
-        posSum.add( boid.position );
+      if (distance > 0 && distance <= _neighborhoodRadius) {
+        posSum.add(boid.position);
         count++;
       }
     }
 
-    if ( count > 0 ) {
-      posSum.divideScalar( count );
+    if (count > 0) {
+      posSum.divideScalar(count);
     }
 
-    steer.subVectors( posSum, this.position );
+    steer.subVectors(posSum, this.position);
     var l = steer.length();
 
-    if ( l > _maxSteerForce ) {
-      steer.divideScalar( l / _maxSteerForce );
+    if (l > _maxSteerForce) {
+      steer.divideScalar(l / _maxSteerForce);
     }
 
     return steer;
   }
 
-  this.alignment = function ( boids ) {
+  this.alignment = function (boids) {
     var boid, velSum = new THREE.Vector3(),
     count = 0;
 
@@ -67,10 +72,10 @@ var Boid = function() {
       if ( Math.random() > 0.6 ) continue;
 
       boid = boids[ i ];
-      distance = boid.position.distanceTo( this.position );
+      distance = boid.position.distanceTo(this.position);
 
-      if ( distance > 0 && distance <= _neighborhoodRadius ) {
-        velSum.add( boid.velocity );
+      if (distance > 0 && distance <= _neighborhoodRadius) {
+        velSum.add(boid.velocity);
         count++;
       }
     }
@@ -87,11 +92,55 @@ var Boid = function() {
     return velSum;
   }
 
+  this.avoidWalls = function(boids) {
+    vector.set( - _width, this.position.y, this.position.z );
+    vector = this.avoid( vector );
+    vector.multiplyScalar( 5 );
+    _acceleration.add( vector );
+
+    vector.set( _width, this.position.y, this.position.z );
+    vector = this.avoid( vector );
+    vector.multiplyScalar( 5 );
+    _acceleration.add( vector );
+
+    vector.set( this.position.x, - _height, this.position.z );
+    vector = this.avoid( vector );
+    vector.multiplyScalar( 5 );
+    _acceleration.add( vector );
+
+    vector.set( this.position.x, _height, this.position.z );
+    vector = this.avoid( vector );
+    vector.multiplyScalar( 5 );
+    _acceleration.add( vector );
+
+    vector.set( this.position.x, this.position.y, - _depth );
+    vector = this.avoid( vector );
+    vector.multiplyScalar( 5 );
+    _acceleration.add( vector );
+
+    vector.set( this.position.x, this.position.y, _depth );
+    vector = this.avoid( vector );
+    vector.multiplyScalar( 5 );
+    _acceleration.add( vector );
+  }
+
+  this.avoid = function(target) {
+    var steer = new THREE.Vector3();
+
+    steer.copy(this.position);
+    steer.sub(target);
+
+    steer.multiplyScalar(1/this.position.distanceToSquared(target));
+
+    return steer;
+  }
+
   this.flock = function (boids) {
     _acceleration.add(this.separation(boids));
     _acceleration.add(this.cohesion(boids));
-    //_acceleration.add(this.alignment(boids));
+    _acceleration.add(this.alignment(boids));
 
+    this.avoidWalls(boids);
     this.velocity.add( _acceleration );
 
     var l = this.velocity.length();
