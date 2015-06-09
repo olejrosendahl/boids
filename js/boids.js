b496a54251c861b6293074c72beb6a12ff3240d8
llPekoll <yoyo.mepa@gmail.com>
2015-06-09 17:40:16 +0700

    add some shadows

--- js/boids.js
+++ js/boids.js
@@ -88,7 +88,7 @@ function init() {
     });
   });
 
-  var geometry = new THREE.BoxGeometry( 50, 50, 50 );
+var geometry = new THREE.BoxGeometry( 50, 50, 50 );
 var material = new THREE.MeshLambertMaterial( { color: 0x00ff00 } );
 var cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
